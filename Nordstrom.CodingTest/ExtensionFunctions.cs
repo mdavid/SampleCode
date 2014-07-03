ace754c8340f87f67cfa03f8cce14230762c659c
M. David Peterson <m.david@3rdandurban.com>
2014-07-03 02:04:27 -0700

    Initial commit of coding test project for Nordstrom. This includes the initial basic functionality for the test with LR and RL designations implemented.

---
+++ Nordstrom.CodingTest/ExtensionFunctions.cs
@@ -0,0 +1,62 @@
+﻿// 
+// ExtensionFunctions.cs
+//  
+// Author:
+//       M. David Peterson <m.david@3rdandurban.com>
+// 
+// Copyright (c) 2014 M. David Peterson
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Text;
+
+namespace Nordstrom.CodingTest
+{
+	public static class StringExtensionMethods
+	{
+		public static string Reverse (this string line)
+		{
+			if (string.IsNullOrEmpty (line))
+				return line;
+			int pivotPosition = line.Length / 2;
+			for (int i = 0; i < pivotPosition; i++) {
+				line = line.Insert (line.Length - i, line.Substring (i, 1)).Remove (i, 1);
+				line = line.Insert (i, line.Substring (line.Length - (i + 2), 1)).Remove (line.Length - (i + 1), 1);
+			}
+			return line;
+		}
+
+		public static void Reverse(this StringBuilder builder)
+		{
+			if (builder.Length  > 1)
+			{
+				int pivotPosition = builder.Length / 2;
+				for (int i = 0; i < pivotPosition; i++)
+				{
+					int iRight     = builder.Length - (i + 1);
+					char rightChar = builder[i];
+					char leftChar  = builder[iRight];
+					builder[i]        = leftChar;
+					builder[iRight]   = rightChar;
+				}
+			}
+		}
+	}
+}
---
+++ Nordstrom.CodingTest/Program.cs
@@ -0,0 +1,126 @@
+﻿// 
+// Program.cs
+//  
+// Author:
+//       M. David Peterson <m.david@3rdandurban.com>
+// 
+// Copyright (c) 2014 M. David Peterson
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/*
+@Puzzle Instructions
+
+Word Search Puzzle
+
+Given a collection of letters which contain hidden words (in the file WORDSEARCH.TXT), find all of the words
+in the word list (in the file WORDLIST.TXT) within the puzzle.  Remember, the words may be hidden left to right,
+right to left, up, down or diagonally.
+
+Create a console application which will search the puzzle for the words contained in the list.  Your output should
+note which word you found, where you found it and one of the following designations for the direction the word takes:
+
+LR – Left to right
+RL – Right to left
+U – Up
+D – Down
+DUL – Diagonal up left
+DUR – Diagonal up right
+DDL – Diagonal down left
+DDR – Diagonal down right
+
+Your deliverable for this exercise will be the console application and all supporting modules necessary to compile
+and run your solution.
+
+There is no “right” solution to this problem.  We are simply trying to determine how you approach the problem, how
+comfortable you are with C# and the quality of the output you deliver.  Solid, well performing code always wins out
+over clever code.  If you have any questions about this exercise, please contact me at dave.ferreira@nordstrom.com.
+
+Good luck and happy coding!
+*/
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Nordstrom.CodingTest
+{
+	class WordSearch
+	{
+		/*
+		LR – Left to right
+		RL – Right to left
+		U – Up
+		D – Down
+		DUL – Diagonal up left
+		DUR – Diagonal up right
+		DDL – Diagonal down left
+		DDR – Diagonal down right
+		*/
+
+		public static void Main (string[] args)
+		{
+			string wordListFile = @".\WordList.txt";
+			string wordSearchFile = @".\WordSearch.txt";
+			IEnumerable<string> wordList = ReadLinesFromFile (wordListFile);
+			IEnumerable<string> wordSearch = ReadLinesFromFile (wordSearchFile);
+
+			foreach (string line in wordSearch) {
+
+				foreach (string word in wordList) {
+
+					bool lineContainsWord = line.Contains (word);
+
+					if (lineContainsWord) {
+						Console.WriteLine ("The word {0} was found LR in the line {1}", word, line);
+					}
+
+					string reverseLine = line.Reverse ();
+					bool reverseLineContainsWord = reverseLine.Contains (word);
+
+					if (reverseLineContainsWord) {
+						Console.WriteLine ("The word {0} was found RL in the line {1}", word, reverseLine);
+					}
+				}
+			}
+
+			Console.Read ();
+		}
+
+		static IEnumerable<string> ReadLinesFromFile (string fileName)
+		{
+			using (StreamReader reader = new StreamReader (fileName, Encoding.UTF8, true)) {
+				while (true) {
+					string line = reader.ReadLine ();
+					if (string.IsNullOrEmpty(line))
+						break;
+					yield return line.ToUpperInvariant ().Replace (" ", "");
+				}
+			}
+		}
+
+		static IEnumerable<bool> ContainsWord (IEnumerable<string> lines, string word)
+		{
+			return from line in lines
+			       select line.Contains (word);
+		}
+	}
+}
